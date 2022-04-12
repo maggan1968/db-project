079e77f0dbecdb51485c5d44d95bb1b9e7bdf745
Malgorzata Altunsayar <mkowalczyk42@gmail.com>
2022-04-12 13:49:42 -0500

    SimpleNavigation

---
+++ src/test/java/com/cydeo/day1/SimpleNavigation.java
@@ -0,0 +1,87 @@
+package com.cydeo.day1;
+
+import java.sql.*;
+
+public class SimpleNavigation {
+    public static void main(String[] args) throws SQLException {
+
+        //step 1 : create connection by providing Connection information
+        //this connection jdbc url, also known as connection string
+        //syntax look like this jdbc:DataBaseType:subprotocal:@Host:port:SID
+
+        String url = "jdbc:oracle:thin:@3.83.127.158:1521:XE";
+        String username = "hr";
+        String password = "hr";
+
+        // now JDBC Provide utility class called DriverManager with many methods
+        // the getConnection method accept 3 parameters url , username, password
+        // according this information ,
+        // it will first look for the driver of the database type you are connecting to
+        // once it's found , use the rest of connection information like
+        // host ip , username password , port , sid to make connection
+        // MAKE SURE YOUR CONNECTION WORK MANUALLY ,
+        // ANYTHING DOES NOT WORK MANUALLY WILL NOT WORK AUTOMATION
+
+        Connection connection = DriverManager.getConnection(url,username,password);
+        System.out.println("Successfully Connected");
+
+        //once we have the connection, now we use this connection object to create statement object
+        Statement statement = connection.createStatement();
+
+        //once we have statement object, now we can execute query to get ResultSet
+        // ResultSet Object store all the row data
+        //It uses iterator to move through the row using the cursor to hold position where it's currently at
+        ResultSet resultSet = statement.executeQuery("SELECT *FROM REGIONS");
+        //By default, the cursor(pointer) of the ResultSet object is at
+        //a location knows as BeforeFirst, basically right before the first row
+        //in order to get the first row, you need to call next() method
+        //NEXT METHOD MOVE YOUR POINTER TO NEXT ROW
+        //
+
+        resultSet.next(); //now we are at first row
+
+        //the resulting data has two column REGION_ID, REGION_NAME
+        //IN ORDER TO access the cell value,
+        //we use getX(X here is data type) method that accept 1 parameter
+        //either using column name or using 1 based index
+        // getString , getInt , getDouble and so on.. we will be just using getString for now
+        System.out.println("rs.getString(\"REGION_ID\") = "
+                + resultSet.getString("REGION_ID"));
+
+        System.out.println("resultSet.getString(\"REGION_NAME\") = "
+                + resultSet.getString("REGION_NAME"));
+
+        //YOU can also use column index
+        System.out.println("resultSet.getString(1) = " + resultSet.getString(1));
+        System.out.println("resultSet.getString(2) = " + resultSet.getString(2));
+
+        //now we now move to next row
+        resultSet.next();// NOW WE ARE AT 2TH ROW
+        System.out.println("rs.getString(\"REGION_ID\") = "
+                + resultSet.getString("REGION_ID"));
+
+        System.out.println("resultSet.getString(\"REGION_NAME\") = "
+                + resultSet.getString("REGION_NAME"));
+
+        //now we now move to next row
+        resultSet.next();// NOW WE ARE AT 3TH ROW
+        System.out.println("rs.getString(\"REGION_ID\") = "
+                + resultSet.getString("REGION_ID"));
+
+        System.out.println("resultSet.getString(\"REGION_NAME\") = "
+                + resultSet.getString("REGION_NAME"));
+
+        //now we now move to next row
+        resultSet.next();// NOW WE ARE AT 4TH ROW
+        System.out.println("rs.getString(\"REGION_ID\") = "
+                + resultSet.getString("REGION_ID"));
+
+        System.out.println("resultSet.getString(\"REGION_NAME\") = "
+                + resultSet.getString("REGION_NAME"));
+
+//we are currently at last row and resultSet.next()will return false if called igain
+        System.out.println("resultSet.next() = " + resultSet.next());//runs false because we don't have more rows
+//now try to get the cell
+    }
+}
+

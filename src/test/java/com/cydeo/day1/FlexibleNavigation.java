e28ab34839e1ea5d2ae2a70a99455bb52923d2d4
Malgorzata Altunsayar <mkowalczyk42@gmail.com>
2022-04-12 14:37:56 -0500

    FlexibleNavigation of ResultSet methods

--- src/test/java/com/cydeo/day1/FlexibleNavigation.java
+++ src/test/java/com/cydeo/day1/FlexibleNavigation.java
@@ -12,7 +12,41 @@ public class FlexibleNavigation {
 
 
             Connection connection = DriverManager.getConnection(url, username, password);
-            Statement statement = connection.createStatement();
+            //creating statement this way will only allow to
+        //get ResultSet tht forward only when we executeQuery
+
+        //Statement statement = connection.createStatement();
+        //in order to be able to move through any row in the resultSet
+        //with the help of ResultSet navigation method like next previous absolute
+        //we need to create Statement object in below way
+
+        Statement statement = connection.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE,ResultSet.CONCUR_READ_ONLY);
             ResultSet resultSet = statement.executeQuery("SELECT * FROM REGIONS");
+
+        /**
+         * In order to access data in ResultSet object , we need to use cursor navigation methods :
+         *
+         * next() -move to next row and return true false according to if we have valid row
+         * previous() -move to previous row and return true false according to if we have valid row
+         * first() - move to first row from anywhere
+         * last() - move to last row from anywhere
+         * beforeFirst() - move to before first location from anywhere
+         * afterLast() - move to after last location from anywhere
+         * absolute(8) - move to any row by using row number , for example 8 in this case
+         */
+
+        resultSet.next() ; // row 1
+        System.out.println("resultSet.next() :" + resultSet.getString("REGION_ID") +" "+ resultSet.getString("REGION_NAME"));
+
+        resultSet.last() ; // last row , row 4
+        System.out.println("resultSet.last() :" +resultSet.getString("REGION_ID") +" "+ resultSet.getString("REGION_NAME"));
+
+        resultSet.first(); // row 1
+        System.out.println("resultSet.first() :" +resultSet.getString("REGION_ID") +" "+ resultSet.getString("REGION_NAME"));
+
+        resultSet.absolute(3); // row 3
+        System.out.println("resultSet.absolute(3) :" +resultSet.getString("REGION_ID") +" "+ resultSet.getString("REGION_NAME"));
+
+
     }
 }

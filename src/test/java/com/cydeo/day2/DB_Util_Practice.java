4d5485c339fc5040a928061dab884832f159e9c2
Malgorzata Altunsayar <mkowalczyk42@gmail.com>
2022-04-14 14:36:55 -0500

    DB_Util_Practice

--- src/test/java/com/cydeo/day2/DB_Util_Practice.java
+++ src/test/java/com/cydeo/day2/DB_Util_Practice.java
@@ -23,11 +23,38 @@ public class DB_Util_Practice {
         // GET THE COUNTRY_NAME FROM THE 2ND ROW
         System.out.println("DB_Util.getCellValue(2, \"COUNTRY_NAME\") = " + DB_Util.getCellValue(2, "COUNTRY_NAME"));
 
+        // run different query here:SELECT * FROM REGION;
+       // DB_Util.runQuery("SELECT * FROM REGION");
+
         // GET ROW COUNT
         System.out.println("DB_Util.getRowCount() = " + DB_Util.getRowCount());
 
-        //get column row
+        //get column count
         System.out.println("DB_Util.getColumnCount() = " + DB_Util.getColumnCount());
+
+        //get all column names as list
+        System.out.println("DB_Util.getAllColumnNamesAsList() = " + DB_Util.getAllColumnNamesAsList());
+
+        // get entire 3tr row as list
+        System.out.println("DB_Util.getRowDataAsList(3) = " + DB_Util.getRowDataAsList(3));
+
+        // get the entire second column as list of string
+        System.out.println("DB_Util.getColumnDataAsList(2) = " + DB_Util.getColumnDataAsList(2));
+
+        //get the COUNTRY_NAME COLUMN AS LIST OF string
+        System.out.println("DB_Util.getColumnDataAsList(\"COUNTRY_NAME\") = " + DB_Util.getColumnDataAsList("COUNTRY_NAME"));
+
+        //get second row as Map<String,String>
+        System.out.println("DB_Util.getRowMap(2) = " + DB_Util.getRowMap(2));
+
+        //get all rows as List of Map
+        System.out.println("DB_Util.getAllRowAsListOfMap() = " + DB_Util.getAllRowAsListOfMap());
+
+        //run this query and get the only cell in the result
+        // SELECT COUNT(*) COUNT FROM EMPLOYEES
+        DB_Util.runQuery("SELECT COUNT(*) COUNT FROM EMPLOYEES");
+        System.out.println("DB_Util.getFirstRowFirstColumn() = " + DB_Util.getFirstRowFirstColumn());
+
 
         //This will clean up ResultSet, Statement, Connection object resources just like you destroyed
         DB_Util.destroy();

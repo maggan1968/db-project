78b42e272a4ac59534849356741e89e3084480c9
Malgorzata Altunsayar <mkowalczyk42@gmail.com>
2022-04-14 11:46:02 -0500

    DAY2 DB_Util_Practice

---
+++ src/test/java/com/cydeo/day2/DB_Util_Practice.java
@@ -0,0 +1,13 @@
+package com.cydeo.day2;
+
+public class DB_Util_Practice {
+
+    public static void main(String[] args) {
+
+        String url = "jdbc:oracle:thin:@3.83.127.158:1521:XE" ;
+        String username = "hr" ;
+        String password = "hr" ;
+
+
+    }
+}

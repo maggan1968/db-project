7f9c9c0bff8cbeae0c91563e1a925d3880083435
Malgorzata Altunsayar <mkowalczyk42@gmail.com>
2022-04-14 14:37:26 -0500

    LibraryDashboardTest

--- src/test/java/com/cydeo/day2/LibraryDashboardTest.java
+++ src/test/java/com/cydeo/day2/LibraryDashboardTest.java
@@ -1,6 +1,14 @@
 package com.cydeo.day2;
 
+import com.cydeo.utility.DB_Util;
 import org.testng.annotations.Test;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+import static org.testng.Assert.assertEquals;
 
 public class LibraryDashboardTest {
 
@@ -8,7 +16,31 @@ public class LibraryDashboardTest {
     @Test
     public void testDashboardNumbers(){
 
+        // Dashboard numbers are previously not matching the database numbers
+        // the test is meant to check
+        // the actual results from dashboard page matches database results
 
+        //do your UI thing here and get the actual count
+        //we will just hard code it for now
+        int actualBookCount = 972;
+
+        //expected result we can get from running this query
+        //SELECT COUNT(*) AS BOOK_COUNT FROM books
+
+        String url = "jdbc:mysql://34.230.35.214:3306/library2";
+        String username = "library2_client";
+        String password = "6s2LQQTjBcGFfDhY";
+
+        DB_Util.createConnection(url, username, password);
+        DB_Util.runQuery("SELECT COUNT(*) AS BOOK_COUNT FROM books");
+        int expectedResult= Integer.parseInt(DB_Util.getFirstRowFirstColumn());
+
+
+        //import static org.testng.Assert.assertEquals;
+        assertEquals(actualBookCount,expectedResult);
+
+        //destroy your connection
+        DB_Util.destroy();
     }
 
 
